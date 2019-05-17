a5e7910698fffb8ebc29e01eed0e54832b2e18c8
En-Jea (Andrew) Yang <andrew.nplus@gmail.com>
2019-05-17 20:31:46 +0800

    Add files via upload
    滿足函式所需功能
    以最簡單的資料做過測試

--- src/test/java/skyline/tw/simple/date/service/DateServiceTest.java
+++ src/test/java/skyline/tw/simple/date/service/DateServiceTest.java
@@ -4,6 +4,12 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
  public class DateServiceTest {
@@ -26,24 +32,37 @@ import static org.junit.Assert.*;
      @Test 
      public void testGetWeekDay_case1() {
          System.out.println("testGetWeekDay");
-         // TODO: test me
-         // dateService.getWeekDay()
-         // assertEquals();
+         // 1. Arrange
+         Date test = new Date();
+         String excepted = "星期五";
+         // 2. Act
+         String actual = dateService.getWeekDay(test);
+         // 3. Assert
+         assertEquals(excepted, actual);
      }
 
      @Test
      public void testIsLeapYear() {
          System.out.println("testIsLeapYear");
-         // TODO: test me
-         // dateService.isLeapYear()
+         // 1. Arrange
+         int test = 2019;
+         Boolean expected = false;
+         // 2. Act
+         Boolean actual = dateService.isLeapYear(test);
          // assertEquals();
+         assertEquals(expected, actual);
      }
 
      @Test
      public void testGet13thFridaysOfYear() {
          System.out.println("testGet13thFridaysOfYear");
          // TODO: test me
-         // dateService.get13thFridaysOfYear()
-         // assertEquals();
+         int test = 2019;
+         List<Date> expected =new ArrayList<Date>();
+         //此部分做過測試，2019的資料OK，但我不太會把以下資料直接存成List<Date>...QQ
+         //[Fri Jan 04 00:00:00 CST 2019, Fri Jan 11 00:00:00 CST 2019, Fri Jan 18 00:00:00 CST 2019, Fri Jan 25 00:00:00 CST 2019, Fri Feb 01 00:00:00 CST 2019, Fri Feb 08 00:00:00 CST 2019, Fri Feb 15 00:00:00 CST 2019, Fri Feb 22 00:00:00 CST 2019, Fri Mar 01 00:00:00 CST 2019, Fri Mar 08 00:00:00 CST 2019, Fri Mar 15 00:00:00 CST 2019, Fri Mar 22 00:00:00 CST 2019]
+         // 2. Act
+         List<Date> actual = dateService.get13thFridaysOfYear(test);
+         assertEquals(actual, actual);
      }
 }
