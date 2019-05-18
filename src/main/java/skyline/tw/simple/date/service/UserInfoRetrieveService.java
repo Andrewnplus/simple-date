4f7969d8e75186d6ef1f37a57979093ba267d9ee
tension huang <yuyuhuang0711@gmail.com>
2019-05-18 21:05:55 +0800

    1. add lombok and log4j 2. add multi-thread test 3. remove unused target folder 4. add .gitignore

--- src/main/java/skyline/tw/simple/date/service/UserInfoRetrieveService.java
+++ src/main/java/skyline/tw/simple/date/service/UserInfoRetrieveService.java
@@ -1,5 +1,6 @@
 package skyline.tw.simple.date.service;
 
+import org.joda.time.DateTime;
 import skyline.tw.simple.date.model.User;
 
 import java.text.DateFormat;
@@ -22,23 +23,22 @@ public class UserInfoRetrieveService {
      * @return
      */
     private DateService dateService;
-    // String, int, double, Map List Set
-    public Map<Integer, String> getWeekDayOfBirthday(User user){
-        Map<Integer, String> ans = new HashMap<>();
-        Date oneYearBirthday = user.birthday;
-        LocalDate localDate = oneYearBirthday.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        int startYear  = localDate.getYear();
-        int month = localDate.getMonthValue();
-        int day   = localDate.getDayOfMonth();
 
-        for ( int i =0; i<=100; i++){
+    public UserInfoRetrieveService(DateService dateService) {
+
+        this.dateService = dateService;
+    }
+
+    // String, int, double, Map List Set
+    public Map<Integer, String> getWeekDayOfBirthday(User user) {
+        Map<Integer, String> results = new HashMap<>();
+        DateTime oneYearBirthday = new DateTime(user.birthday);
+        int startYear = oneYearBirthday.getYear();
+        for (int i = 0; i <= user.getAge(); i++) {
             int year = i + startYear;
-            Date date = new GregorianCalendar(year, month, day).getTime();
-            DateFormat format2=new SimpleDateFormat("EEEE");
-            String finalDay=format2.format(date);
-            ans.put(i, finalDay);
+            results.put(i, dateService.getWeekDay(new DateTime(oneYearBirthday).withYear(year).toDate()));
         }
-        return ans;
+        return results;
     }
 
 }
---
+++ src/test/java/skyline/tw/simple/date/service/MultiThreadTest.java
@@ -0,0 +1,55 @@
+package skyline.tw.simple.date.service;
+
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import skyline.tw.simple.date.model.User;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+@Log4j
+public class MultiThreadTest {
+
+    @Test
+    public void test() throws Exception {
+
+        log.info("Main thread starts!!!!!!!");
+        DateService dateService = new DateService();
+        UserInfoRetrieveService userInfoRetrieveService = new UserInfoRetrieveService(dateService);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        User john = new User(dateFormat.parse("1996/01/01"), "John", "Mayer", "GuitarGod");
+        User mary = new User(dateFormat.parse("1994/07/01"), "Mary", "Mayer", "GuitarGod");
+        User mark = new User(dateFormat.parse("2000/12/01"), "Mark", "Mayer", "GuitarGod");
+
+        Thread t1 = new Thread(new MyRunnable(userInfoRetrieveService, john));
+        Thread t2 = new Thread(new MyRunnable(userInfoRetrieveService, mark));
+        Thread t3 = new Thread(new MyRunnable(userInfoRetrieveService, mary));
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        log.info("Main thread is ready to leave!!!!!!!");
+    }
+
+
+    private static class MyRunnable implements Runnable {
+
+        private UserInfoRetrieveService userInfoRetrieveService;
+        private User user;
+
+        public MyRunnable(UserInfoRetrieveService dateService, User user){
+            this.userInfoRetrieveService = dateService;
+            this.user = user;
+        }
+
+        @Override
+        public void run() {
+            log.info(user);
+            log.info(userInfoRetrieveService.getWeekDayOfBirthday(user));
+        }
+    }
+}
